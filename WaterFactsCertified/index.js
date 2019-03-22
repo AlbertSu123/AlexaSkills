a714b11eae1caf8b0344a820b8a5921ece67eb81
Albert Su <albertsu2001@gmail.com>
2019-03-22 13:19:26 -0700

    Update index.js

--- WaterFactsCertified/index.js
+++ WaterFactsCertified/index.js
@@ -44,14 +44,17 @@ const languageStrings = {
     'en-US': {
         translation: {
             FACTS: [
-                'Everyone who has died has consumed Water.',
-                'The atomic weight of H2O is 18',
-                'Di-Hydrogen Monoxide will kill you',
                 'Nearly 97% of the world is satlty or undrinkable.',
                 '2% of water is ice',
+                'NASA scientists predict that there is an 80 percent chance of a megadrought in the Southwest United States before the end of the century.',
                 'Nearly one-half of the water used by Americans is used for thermoelectric power generation',
                 'Water can dissolve more substances than any other liquid including sulfuric acid.',
                 'About 6,800 gallons of water is required to grow a day’s food for a family of four.',
+                'Unsafe water kills 200 children every hour.',
+                'The drought currently encompasses over 98% of the state of California.',
+                'Farmers could sell their water for $700 an acre foot, more than they would earn by using the water to grow crops.',
+                'California is the world’s fifth-largest supplier of food.',
+                'The current drought cost the (farming) sector an estimated $2.2 billion last year.',f four.',
                 'Unsafe water kills 200 children every hour.'
             ],
             SKILL_NAME: 'Water Facts',
@@ -60,15 +63,17 @@ const languageStrings = {
     'en-GB': {
         translation: {
             FACTS: [
-                'Everyone who has died has consumed Water.',
-                'The atomic weight of H2O is 18',
-                'Di-Hydrogen Monoxide will kill you',
                 'Nearly 97% of the world is satlty or undrinkable.',
                 '2% of water is ice',
+                'NASA scientists predict that there is an 80 percent chance of a megadrought in the Southwest United States before the end of the century.',
                 'Nearly one-half of the water used by Americans is used for thermoelectric power generation',
                 'Water can dissolve more substances than any other liquid including sulfuric acid.',
                 'About 6,800 gallons of water is required to grow a day’s food for a family of four.',
-                'Unsafe water kills 200 children every hour.'
+                'Unsafe water kills 200 children every hour.',
+                'The drought currently encompasses over 98% of the state of California.',
+                'Farmers could sell their water for $700 an acre foot, more than they would earn by using the water to grow crops.',
+                'California is the world’s fifth-largest supplier of food.',
+                'The current drought cost the (farming) sector an estimated $2.2 billion last year.',
             ],
             SKILL_NAME: 'British Space Facts',
         },
@@ -76,7 +81,17 @@ const languageStrings = {
     'de': {
         translation: {
             FACTS: [
-'Gutentag, no comprendo '
+                "Fast 97% der Welt ist salzig oder nicht trinkbar.",
+                "2% Wasser ist Eis",
+                "Wissenschaftler der NASA sagen voraus, dass es in den Südwesten der Vereinigten Staaten vor dem Ende des Jahrhunderts eine 80-prozentige Chance für einen Mega-Kauf gibt."
+                "Fast die Hälfte des von Amerikanern verwendeten Wassers wird für die thermoelektrische Stromerzeugung verwendet",
+                "Wasser kann mehr Substanzen auflösen als jede andere Flüssigkeit, einschließlich Schwefelsäure."
+                "Um eine tägliche Nahrung für eine vierköpfige Familie anzubauen, sind ungefähr 6.800 Gallonen Wasser erforderlich.",
+                "Unsicheres Wasser tötet 200 Kinder pro Stunde.",
+                "Die Dürre umfasst derzeit über 98% des Staates Kalifornien.",
+                "Landwirte könnten ihr Wasser für 700 US-Dollar pro Hektar verkaufen, mehr als sie mit dem Wasser für den Anbau von Getreide verdienen würden."
+                "Kalifornien ist der weltweit fünftgrößte Anbieter von Lebensmitteln.",
+                "Die derzeitige Dürre kostete den (Landwirtschafts-) Sektor im vergangenen Jahr geschätzte 2,2 Milliarden US-Dollar
             ],
             SKILL_NAME: 'Weltraumwissen auf Deutsch',
             GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
